d1be618ecbeca0564aa5a38e28528eeef7cf565d
Johnny Marcelino <100697981+johnnymarcelino@users.noreply.github.com>
2022-04-26 10:53:35 -0300

    New commit stopping in challenge 73

--- Mundo-03.py
+++ Mundo-03.py
@@ -41,14 +41,26 @@ print(pessoa)'''
 # Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, do zero até vinte.
 # Seu programa deverá ler um numero pelo teclado (entre 0 e 20) e mostrá-lo por extenso
 
-numExtenso = ("Zero", "One", "Two", "Tree", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+'''numExtenso = ("Zero", "One", "Two", "Tree", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
               "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty")
 numeral = int(input("Enter any number between '0' and '20': "))
 if(numeral > 20):
     while(numeral > 20):
         numeral = int(input("Enter any number between '0' and '20': "))
 
-print(f"You have choose {numeral} in numeral and this number in extensive is {numExtenso[numeral]}")
+print(f"You have choose {numeral} in numeral and this number in extensive is {numExtenso[numeral]}")'''
 
-# CHALLENGE 72
+# CHALLENGE 73
 
+times = ("São Paulo", "Vasco", "Botafogo", "Cruzeiro", "Flamengo", "Coritiba", "Juventude", "Arsenal", "Man Unt", "Milan", "Liverpool",
+              "Inter Milão", "Real Madrid", "Barcelona", "Bayer Muniq", "Chelsea", "PSG", "Palmeiras", "Corinthians", "Zenit", "Man City")
+rank = 0
+print(f"These are the rankings of the top 5 on the soccer table:\n{times[0:5]}")
+print(f"These are the rankings of the last 4 on the soccer table:\n{times[-4:len(times):1]}")
+print(f"This is the list of the club's classification in sort {sorted(times)}")
+for pos in times:
+    if(pos != "Barcelona"):
+        rank += 1
+    if(pos == "Barcelona"):
+        break
+print(f"Barcelona club it is in the position {times.index(pos)}ª of the classification!")
