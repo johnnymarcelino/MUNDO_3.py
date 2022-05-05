bafaad3543c2d4b72b2186116cdadeed52e8f823
Johnny Marcelino <100697981+johnnymarcelino@users.noreply.github.com>
2022-05-05 11:05:48 -0300

    TRABALHANDO COM LISTAS - FUNÇÃO FOR E LISTA

--- Progressivo.py
+++ Progressivo.py
@@ -45,9 +45,39 @@
 # print(combs)
 
 # value ['3.1', '3.14', '3.142', '3.1416', '3.14159']
-from math import pi
+# from math import pi
 # a = [str(round(pi, i)) for i in range(1, 6)]
-a = []
-for i in range(1, 6):
-    a.append(str(round(pi, i)))
-print(a, end=" ")
+# a = []
+# for i in range(1, 6+1):
+#     a.append(str(round(pi, i)))
+# print(a)
+
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+# # print(len(matrix[0]))
+# for row in matrix:
+#     # print(len(row))
+#     for i in range(4):
+#         matrix.append([row[i]])
+# print(matrix)
+# matrix = []
+# [[row[i] for row in matrix] for i in range(4)]]
+
+# transposed = []
+# for i in range(4):
+#     transposed.append([row[i] for row in matrix])
+# print(transposed)
+
+# [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
+# transposed = []
+# for i in range(4):
+#     transposed_row = []
+#     for row in matrix:
+#         transposed_row.append(row[i])
+#     transposed.append(transposed_row)
+# print(transposed)
+
+# a = [-1, 1, 66.25, 333, 333, 1234.5]
+# del(a[0])
+# print(a)
+# del(a[2:5])
+# print(a)
