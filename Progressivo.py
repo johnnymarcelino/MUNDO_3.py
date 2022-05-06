26a9c0d24377a263e2a3d463a8e1600dc67e7484
Johnny Marcelino <100697981+johnnymarcelino@users.noreply.github.com>
2022-05-06 16:44:04 -0300

    TRABALHANDO COM LISTAS - FUNÇÃO FOR E LISTA

--- Progressivo.py
+++ Progressivo.py
@@ -136,6 +136,173 @@
 #     media += valor
 # for b in range(len(nota)):
 #     print(f"A sua nota em {disciplinas[b]}: {nota[b]}")
-# print(f"Portanto a sua média geral é de {media/materias}")
-# # for media in nota:
-# #     print("Portanto, a sua médi {}")
+# print(f"Portanto a sua média geral é de {media/materias:.2f}")
+# for media in nota:
+#     print("Portanto, a sua médi {}")
+
+
+# print("SEQUENCIA DE FIBONACCI")
+#
+# numero = int(input("Digite aqui um numero: "))
+# termos = []
+# for c in range(0, numero):
+#     if(c == 0 or c == 1):
+#         termos.append(c)
+#     else:
+#         termos.append(termos[c-1]+termos[c-2])
+# print(termos)
+
+
+# semana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"]
+# novaLista = semana[1:5]
+# print(novaLista)
+
+# if("Domingo" in semana):
+#     print("Sim", 'Domingo', "Está em semana!")
+# else:
+#     print("Não está!")
+
+# if("Domingo" not in semana):
+#     print("True")
+# else:
+#     print("False")
+
+
+# list1 = [1, 2, 3, 4]
+# # list2 = list1
+# # list1[1] = 9
+# # # print(list2)
+# # list2 = []
+# # for item in list1:
+# #     list2.append(item)
+# # print(list2)
+# # list2[1] = 2
+# list2 = [] + list1
+# list1[3] = 5
+# print(list1, "E", list2)
+
+
+# matriz = [[0 for i in range(3)] for j in range(3)]
+# count = 0
+# # print(matriz)
+# for linha in range(3):
+#     for coluna in range(3):
+#         matriz[linha][coluna] = count
+#         count += 1
+# print(matriz)
+# print()
+# for linha in range(3):
+#     for coluna in range(3):
+#         print("%4d" % matriz[linha][coluna], end='')
+#     print()
+
+
+# board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# board = [[0 for i in range(3)] for j in range(3)]
+# continuar = 1
+# jogada = 0
+# jogador = jogada % 2 + 1
+# while continuar:
+#     continuar = int(input("Digite 1 para continuar\n"
+#                           "Digite 0 para parar: "))
+#     print(board)
+#     jogada += 1
+# ///////////////////////////////////////////////
+# JOGO DA VELHA
+# ///////////////////////////////////////////////
+
+# def menu():
+#     continuar = 1
+#     while continuar:
+#         continuar = int(input("0. Sair \n" +
+#                               "1. Jogar novamente\n"))
+#         if continuar:
+#             game()
+#         else:
+#             print("Saindo...")
+#
+#
+# def game():
+#     jogada = 0
+#
+#     while ganhou() == 0:
+#         print("\nJogador ", jogada % 2 + 1)
+#         exibe()
+#         linha = int(input("\nLinha :"))
+#         coluna = int(input("Coluna:"))
+#
+#         if board[linha - 1][coluna - 1] == 0:
+#             if (jogada % 2 + 1) == 1:
+#                 board[linha - 1][coluna - 1] = 1
+#             else:
+#                 board[linha - 1][coluna - 1] = -1
+#         else:
+#             print("Nao esta vazio")
+#             jogada -= 1
+#
+#         if ganhou():
+#             print("Jogador ", jogada % 2 + 1, " ganhou apos ", jogada + 1, " rodadas")
+#
+#         jogada += 1
+#
+#
+# def ganhou():
+#     # checando linhas
+#     for i in range(3):
+#         soma = board[i][0] + board[i][1] + board[i][2]
+#         if soma == 3 or soma == -3:
+#             return 1
+#
+#     # checando colunas
+#     for i in range(3):
+#         soma = board[0][i] + board[1][i] + board[2][i]
+#         if soma == 3 or soma == -3:
+#             return 1
+#
+#     # checando diagonais
+#     diagonal1 = board[0][0] + board[1][1] + board[2][2]
+#     diagonal2 = board[0][2] + board[1][1] + board[2][0]
+#     if diagonal1 == 3 or diagonal1 == -3 or diagonal2 == 3 or diagonal2 == -3:
+#         return 1
+#
+#     return 0
+#
+#
+# def exibe():
+#     for i in range(3):
+#         for j in range(3):
+#             if board[i][j] == 0:
+#                 print(" _ ", end=' ')
+#             elif board[i][j] == 1:
+#                 print(" X ", end=' ')
+#             elif board[i][j] == -1:
+#                 print(" O ", end=' ')
+#
+#         print()
+#
+#
+# board = [[0, 0, 0],
+#          [0, 0, 0],
+#          [0, 0, 0]]
+#
+# menu()
+
+
+# COMBINAÇÕES DE JOGOS DA MEGA SENA
+
+# total=0
+# for dez1 in range(1, 60+1):
+#     print(dez1, end=" ")
+#     for dez2 in range(dez1+1, 60+1):
+#         print(dez2, end=" ")
+#     #     for dez3 in range(dez2+1,60):
+#     #         # print(dez3)
+#     #         for dez4 in range(dez3+1,60):
+#     #             # print(dez4)
+#     #             for dez5 in range(dez4+1,60):
+#     #                 # print(dez5)
+#     #                 for dez6 in range(dez5+1,60):
+#     #                     print(dez6)
+#     total += 1
+# print(total)
+
