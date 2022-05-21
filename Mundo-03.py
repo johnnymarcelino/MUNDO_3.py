980c569bb29aa70d5f5e0f14ce2b86338918c1eb
Johnny Marcelino <100697981+johnnymarcelino@users.noreply.github.com>
2022-05-21 08:37:42 -0300

    CHALLENGE 96 - STARTING - ACTUALLY

--- Mundo-03.py
+++ Mundo-03.py
@@ -1521,3 +1521,11 @@ print(valores)'''
 
 # CHALLENGE 95
 
+
+print("  Controle de Terrenos ")
+print("-"*20)
+def area(l, c):
+    print(f"The area of a land {l}x{c} is {l*c:5.2f}m².")
+
+
+area(float(input("LENGTH (m): ")), float(input("WIDTH (m): ")))
