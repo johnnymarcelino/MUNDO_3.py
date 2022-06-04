ceb2532e923c3fd69bfbda83a231f6c2d4350d69
Johnny Marcelino <100697981+johnnymarcelino@users.noreply.github.com>
2022-06-04 12:16:00 -0300

    ALL CHALLENGES - COMPLETED AND UPDATED

--- Mundo-03.py
+++ Mundo-03.py
@@ -2018,3 +2018,45 @@ while(b != "FIM"):
         break
     # break
 print("Até Logo")'''
+
+# SOLUTION FROM GUANABARA
+
+'''from time import sleep
+c = ("\033[m",         # 0 - sem cores
+       "\033[0;30;41m",  # 1 - Vermelho
+       "\033[0;30;42m",  # 2 - Verde
+       "\033[0;30;43m",  # 3 - Amarelo
+       "\033[0;30;44m",  # 4 - Azul
+       "\033[0;30;45m",  # 5 - Roxo
+       "\033[7;30m"      # 6 - Branco
+       );
+
+
+def ajuda(com):
+    titulo(f"ACESSANDO O MANUAL DO COMANDO \ '{comando}\"", 4)
+    print(c[6], end="")
+    help(com)
+    print(c[0], end="")
+    sleep(1)
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 6
+    print(c[cor], end="")
+    print("~"*tam)
+    print(f"   {msg}")
+    print("~"*tam)
+    print(c[0], end="")
+    sleep(2)
+
+
+# Programa principal
+comando = ""
+while True:
+    titulo("SISTEMA DE AJUDA PyHELP!", 2)
+    comando = str(input("Função ou biblioteca > "))
+    if(comando.upper() == "FIM"):
+        break
+    else:
+        ajuda(comando)
+titulo("ATÉ LOGO!", 1)'''
