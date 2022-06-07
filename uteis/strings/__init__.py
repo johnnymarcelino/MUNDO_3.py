a42de75c89623094c7de7eede17dec1ba51885f3
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-06-07 14:45:10 -0300

    ADDED CLASS 22 | FINISHED AND OTHERS 11 FILES NECESSARY

---
+++ uteis/strings/__init__.py
@@ -0,0 +1,28 @@
+def metade(charge):
+    # print("="*30)
+    # if(troco == "sim"):
+    # print("="*30)
+    half = charge / 2
+    return half
+
+def diminuir(charge):
+    # global totalDec
+    # global ta
+    decrease = charge * (10/100)
+    # def totalDim(charge):
+    #     totalDec = charge - decrease
+    #     return totalDec, decrease
+    # # totalDec = charge - decrease
+    return decrease
+
+
+def dobro(charge):
+    double = charge * 2
+    return double
+
+
+def aumentar(charge):
+    # global totalInc
+    increase = charge * (10/100)
+    # totalInc = charge + increase
+    return increase
