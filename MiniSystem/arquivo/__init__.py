e50bfeab4860619b73b45c48149fbcaf7da5b8c8
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-06-11 17:17:43 -0300

    CHALLENGE 115 - STARTED AND COMPLETED 100%

---
+++ MiniSystem/arquivo/__init__.py
@@ -0,0 +1,40 @@
+from MiniSystem.system import *
+from MiniSystem import *
+import MiniSystem
+# import Mundo
+arq = "FileSystem.txt"
+
+
+# def filExist(nome):
+#     try:
+#         a = open(nome, "rt")
+#         a.close()
+#     except FileNotFoundError:
+#         return False
+#     else:
+#         return True
+#
+#
+#
+# def creatFile(nome):
+#     try:
+#         a = open(nome, "wt+")
+#         a.close()
+#     except:
+#         print("There is some problem with the create the file")
+#     else:
+#         print(f"File {nome}, created with success!")
+#     readFile(arq)
+#
+# def readFile(name):
+#     try:
+#         a = open(name, "rt")
+#     except:
+#         print("There is a problem with the program")
+#     else:
+#         menu("List of People")
+#         print(a.readlines())
+#     option()
+
+
+# readFile("Ola")
